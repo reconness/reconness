17cea07fc0c109a896d4aef9a7707ec8a7c66448
Alejandro Ferrandiz <aferrandiz@niznikhealth.com>
2020-01-08 00:04:53 -0500

    Work on Label UnitTest

--- src/ReconNess.UnitTests/CategoryServiceTests.cs
+++ src/ReconNess.UnitTests/CategoryServiceTests.cs
@@ -15,13 +15,14 @@ namespace ReconNess.UnitTests
     [TestClass]
     public class CategoryServiceTests
     {
-        [TestMethod]
-        public void TestGetCategoriesAsyncNotNewCategoryMethod()
-        {
-            // Arrange
-            var addWasCalled = false;
+        private IUnitOfWork unitOfWork;
+        private Guid categoryIdOnDb;
+        private bool addWasCalled = false;
 
-            var categoryIdOnDb = Guid.NewGuid();
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.categoryIdOnDb = Guid.NewGuid();
             var categoriesOnDb = new List<Category>
             {
                 new Category
@@ -30,20 +31,6 @@ namespace ReconNess.UnitTests
                     Name = "Brute Force"
                 }
             };
-
-            var myCategoriesOnDb = new List<AgentCategory>
-            {
-                new AgentCategory
-                {
-                    Category = new Category
-                    {
-                        Id = categoryIdOnDb,
-                        Name = "Brute Force"
-                    }
-                }
-            };
-
-            var myNewCategories = new List<string> { "Brute Force" };
 
             var repositoryMock = new Mock<IRepository<Category>>();
             repositoryMock.Setup(c => c.GetByCriteriaAsync(It.IsAny<Expression<Func<Category, bool>>>(), It.IsAny<CancellationToken>()))
@@ -56,243 +43,149 @@ namespace ReconNess.UnitTests
             unitOfWorkMock.Setup(c => c.CommitAsync(It.IsAny<CancellationToken>()))
                 .Callback(() =>
                 {
-                    addWasCalled = true;
+                    this.addWasCalled = true;
                 });
 
             unitOfWorkMock.Setup(m => m.Repository<Category>(It.IsAny<CancellationToken>()))
                 .Returns(repositoryMock.Object);
 
-            // Act
-            var categoryService = new CategoryService(unitOfWorkMock.Object);
-            var categorties = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
-
-            // Assert
-            Assert.IsTrue(categorties.Count == 1);
-            Assert.IsTrue(addWasCalled == false);
+            this.unitOfWork = unitOfWorkMock.Object;
         }
 
         [TestMethod]
-        public void TestGetCategoriesAsyncRemoveCategoryMethod()
+        public void GetCategoriesAsync_NotNewCategory()
         {
-            // Arrange
-            var addWasCalled = false;
+            var myNewCategories = new List<string> { "Brute Force" };
 
-            var categoryIdOnDb = Guid.NewGuid();
-            var categoriesOnDb = new List<Category>
-            {
-                new Category
-                {
-                    Id = categoryIdOnDb,
-                    Name = "Brute Force"
-                }
-            };
-
+            // Arrange       
             var myCategoriesOnDb = new List<AgentCategory>
             {
                 new AgentCategory
                 {
                     Category = new Category
                     {
-                        Id = categoryIdOnDb,
+                        Id = this.categoryIdOnDb,
+                        Name = "Brute Force"
+                    }
+                }
+            };   
+
+            var categoryService = new CategoryService(this.unitOfWork);
+
+            // Act
+            var categories = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
+
+            // Assert
+            Assert.IsTrue(categories.Count == 1);
+            Assert.IsTrue(this.addWasCalled == false);
+        }
+
+        [TestMethod]
+        public void GetCategoriesAsync_RemoveCategory()
+        {
+            // Arrange
+            var myCategoriesOnDb = new List<AgentCategory>
+            {
+                new AgentCategory
+                {
+                    Category = new Category
+                    {
+                        Id = this.categoryIdOnDb,
                         Name = "Brute Force"
                     }
                 }
             };
 
             var myNewCategories = new List<string>();
-
-            var repositoryMock = new Mock<IRepository<Category>>();
-            repositoryMock.Setup(c => c.GetByCriteriaAsync(It.IsAny<Expression<Func<Category, bool>>>(), It.IsAny<CancellationToken>()))
-                .Returns<Expression<Func<Category, bool>>, CancellationToken>((f, c) =>
-                {
-                    return Task.FromResult(categoriesOnDb.AsQueryable().FirstOrDefault(f));
-                });
-
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            unitOfWorkMock.Setup(c => c.CommitAsync(It.IsAny<CancellationToken>()))
-                .Callback(() =>
-                {
-                    addWasCalled = true;
-                });
-
-            unitOfWorkMock.Setup(m => m.Repository<Category>(It.IsAny<CancellationToken>()))
-                .Returns(repositoryMock.Object);
+            var categoryService = new CategoryService(this.unitOfWork);
 
             // Act
-            var categoryService = new CategoryService(unitOfWorkMock.Object);
-            var categorties = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
+            var categories = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
 
             // Assert
-            Assert.IsTrue(categorties.Count == 0);
-            Assert.IsTrue(addWasCalled == false);
+            Assert.IsTrue(categories.Count == 0);
+            Assert.IsTrue(this.addWasCalled == false);
         }
 
         [TestMethod]
-        public void TestGetCategoriesAsyncAddNewCategoryMethod()
+        public void GetCategoriesAsync_AddNewCategory()
         {
             // Arrange
-            var addWasCalled = false;
-
-            var categoryIdOnDb = Guid.NewGuid();
-            var categoriesOnDb = new List<Category>
-            {
-                new Category
-                {
-                    Id = categoryIdOnDb,
-                    Name = "Brute Force"
-                }
-            };
-
             var myCategoriesOnDb = new List<AgentCategory>
             {
                 new AgentCategory
                 {
                     Category = new Category
                     {
-                        Id = categoryIdOnDb,
+                        Id = this.categoryIdOnDb,
                         Name = "Brute Force"
                     }
                 }
             };
 
             var myNewCategories = new List<string> { "Brute Force", "New Category" };
-
-            var repositoryMock = new Mock<IRepository<Category>>();
-            repositoryMock.Setup(c => c.GetByCriteriaAsync(It.IsAny<Expression<Func<Category, bool>>>(), It.IsAny<CancellationToken>()))
-                .Returns<Expression<Func<Category, bool>>, CancellationToken>((f, c) =>
-                {
-                    return Task.FromResult(categoriesOnDb.AsQueryable().FirstOrDefault(f));
-                });
-
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            unitOfWorkMock.Setup(c => c.CommitAsync(It.IsAny<CancellationToken>()))
-                .Callback(() =>
-                {
-                    addWasCalled = true;
-                });
-
-            unitOfWorkMock.Setup(m => m.Repository<Category>(It.IsAny<CancellationToken>()))
-                .Returns(repositoryMock.Object);
+            var categoryService = new CategoryService(this.unitOfWork);
 
             // Act
-            var categoryService = new CategoryService(unitOfWorkMock.Object);
-            var categorties = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
+            var categories = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
 
             // Assert
-            Assert.IsTrue(categorties.Count == 2);
-            Assert.IsTrue(addWasCalled == true);
+            Assert.IsTrue(categories.Count == 2);
+            Assert.IsTrue(this.addWasCalled == true);
         }
 
         [TestMethod]
-        public void TestGetCategoriesAsyncAddAllNewCategoryMethod()
+        public void GetCategoriesAsync_AddAllNewCategory()
         {
             // Arrange
-            var addWasCalled = false;
-
-            var categoryIdOnDb = Guid.NewGuid();
-            var categoriesOnDb = new List<Category>
-            {
-                new Category
-                {
-                    Id = categoryIdOnDb,
-                    Name = "Brute Force"
-                }
-            };
-
             var myCategoriesOnDb = new List<AgentCategory>
             {
                 new AgentCategory
                 {
                     Category = new Category
                     {
-                        Id = categoryIdOnDb,
+                        Id = this.categoryIdOnDb,
                         Name = "Brute Force"
                     }
                 }
             };
 
             var myNewCategories = new List<string> { "New Category" };
-
-            var repositoryMock = new Mock<IRepository<Category>>();
-            repositoryMock.Setup(c => c.GetByCriteriaAsync(It.IsAny<Expression<Func<Category, bool>>>(), It.IsAny<CancellationToken>()))
-                .Returns<Expression<Func<Category, bool>>, CancellationToken>((f, c) =>
-                {
-                    return Task.FromResult(categoriesOnDb.AsQueryable().FirstOrDefault(f));
-                });
-
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            unitOfWorkMock.Setup(c => c.CommitAsync(It.IsAny<CancellationToken>()))
-                .Callback(() =>
-                {
-                    addWasCalled = true;
-                });
-
-            unitOfWorkMock.Setup(m => m.Repository<Category>(It.IsAny<CancellationToken>()))
-                .Returns(repositoryMock.Object);
+            var categoryService = new CategoryService(this.unitOfWork);
 
             // Act
-            var categoryService = new CategoryService(unitOfWorkMock.Object);
-            var categorties = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
+            var categories = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
 
             // Assert
-            Assert.IsTrue(categorties.Count == 1);
-            Assert.IsTrue(addWasCalled == true);
+            Assert.IsTrue(categories.Count == 1);
+            Assert.IsTrue(this.addWasCalled == true);
         }
 
         [TestMethod]
-        public void TestGetCategoriesAsyncAddAllNewDuplicateCategoryMethod()
+        public void GetCategoriesAsyncAdd_AllNewDuplicateCategory()
         {
             // Arrange
-            var addWasCalled = false;
-
-            var categoryIdOnDb = Guid.NewGuid();
-            var categoriesOnDb = new List<Category>
-            {
-                new Category
-                {
-                    Id = categoryIdOnDb,
-                    Name = "Brute Force"
-                }
-            };
-
             var myCategoriesOnDb = new List<AgentCategory>
             {
                 new AgentCategory
                 {
                     Category = new Category
                     {
-                        Id = categoryIdOnDb,
+                        Id = this.categoryIdOnDb,
                         Name = "Brute Force"
                     }
                 }
             };
 
             var myNewCategories = new List<string> { "New Category", "New Category" };
-
-            var repositoryMock = new Mock<IRepository<Category>>();
-            repositoryMock.Setup(c => c.GetByCriteriaAsync(It.IsAny<Expression<Func<Category, bool>>>(), It.IsAny<CancellationToken>()))
-                .Returns<Expression<Func<Category, bool>>, CancellationToken>((f, c) =>
-                {
-                    return Task.FromResult(categoriesOnDb.AsQueryable().FirstOrDefault(f));
-                });
-
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            unitOfWorkMock.Setup(c => c.CommitAsync(It.IsAny<CancellationToken>()))
-                .Callback(() =>
-                {
-                    addWasCalled = true;
-                });
-
-            unitOfWorkMock.Setup(m => m.Repository<Category>(It.IsAny<CancellationToken>()))
-                .Returns(repositoryMock.Object);
+            var categoryService = new CategoryService(this.unitOfWork);
 
             // Act
-            var categoryService = new CategoryService(unitOfWorkMock.Object);
-            var categorties = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
+            var categories = categoryService.GetCategoriesAsync(myCategoriesOnDb, myNewCategories).Result;
 
             // Assert
-            Assert.IsTrue(categorties.Count == 1);
-            Assert.IsTrue(addWasCalled == true);
+            Assert.IsTrue(categories.Count == 1);
+            Assert.IsTrue(this.addWasCalled == true);
         }
     }
 }
--- src/ReconNess.UnitTests/LabelServiceTests.cs
+++ src/ReconNess.UnitTests/LabelServiceTests.cs
@@ -1,18 +1,191 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ReconNess.Core;
+using ReconNess.Entities;
+using ReconNess.Services;
 
 namespace ReconNess.UnitTests
 {
     [TestClass]
     public class LabelServiceTests
     {
-        [TestMethod]
-        public void TestGetLabelsAsyncMethod()
+        private IUnitOfWork unitOfWork;
+        private Guid labelIdOnDb;
+        private bool addWasCalled = false;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
-            // Arrange
+            this.labelIdOnDb = Guid.NewGuid();
+            var LabelsOnDb = new List<Label>
+            {
+                new Label
+                {
+                    Id = labelIdOnDb,
+                    Name = "Brute Force"
+                }
+            };
+
+            var repositoryMock = new Mock<IRepository<Label>>();
+            repositoryMock.Setup(c => c.GetByCriteriaAsync(It.IsAny<Expression<Func<Label, bool>>>(), It.IsAny<CancellationToken>()))
+                .Returns<Expression<Func<Label, bool>>, CancellationToken>((f, c) =>
+                {
+                    return Task.FromResult(LabelsOnDb.AsQueryable().FirstOrDefault(f));
+                });
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(c => c.CommitAsync(It.IsAny<CancellationToken>()))
+                .Callback(() =>
+                {
+                    this.addWasCalled = true;
+                });
+
+            unitOfWorkMock.Setup(m => m.Repository<Label>(It.IsAny<CancellationToken>()))
+                .Returns(repositoryMock.Object);
+
+            this.unitOfWork = unitOfWorkMock.Object;
+        }
+
+        [TestMethod]
+        public void GetLabelsAsync_NotNewLabel()
+        {
+            var myNewLabels = new List<string> { "Brute Force" };
+
+            // Arrange       
+            var myLabelsOnDb = new List<SubdomainLabel>
+            {
+                new SubdomainLabel
+                {
+                    Label = new Label
+                    {
+                        Id = this.labelIdOnDb,
+                        Name = "Brute Force"
+                    }
+                }
+            };
+
+            var labelService = new LabelService(this.unitOfWork);
 
             // Act
+            var labels = labelService.GetLabelsAsync(myLabelsOnDb, myNewLabels).Result;
 
             // Assert
+            Assert.IsTrue(labels.Count == 1);
+            Assert.IsTrue(this.addWasCalled == false);
+        }
+
+        [TestMethod]
+        public void GetLabelsAsync_RemoveLabel()
+        {
+            // Arrange
+            var myLabelsOnDb = new List<SubdomainLabel>
+            {
+                new SubdomainLabel
+                {
+                    Label = new Label
+                    {
+                        Id = this.labelIdOnDb,
+                        Name = "Brute Force"
+                    }
+                }
+            };
+
+            var myNewLabels = new List<string>();
+            var labelService = new LabelService(this.unitOfWork);
+
+            // Act
+            var labels = labelService.GetLabelsAsync(myLabelsOnDb, myNewLabels).Result;
+
+            // Assert
+            Assert.IsTrue(labels.Count == 0);
+            Assert.IsTrue(this.addWasCalled == false);
+        }
+
+        [TestMethod]
+        public void GetLabelsAsync_AddNewLabel()
+        {
+            // Arrange
+            var myLabelsOnDb = new List<SubdomainLabel>
+            {
+                new SubdomainLabel
+                {
+                    Label = new Label
+                    {
+                        Id = this.labelIdOnDb,
+                        Name = "Brute Force"
+                    }
+                }
+            };
+
+            var myNewLabels = new List<string> { "Brute Force", "New Label" };
+            var labelService = new LabelService(this.unitOfWork);
+
+            // Act
+            var labels = labelService.GetLabelsAsync(myLabelsOnDb, myNewLabels).Result;
+
+            // Assert
+            Assert.IsTrue(labels.Count == 2);
+            Assert.IsTrue(this.addWasCalled == true);
+        }
+
+        [TestMethod]
+        public void GetLabelsAsync_AddAllNewLabel()
+        {
+            // Arrange
+            var myLabelsOnDb = new List<SubdomainLabel>
+            {
+                new SubdomainLabel
+                {
+                    Label = new Label
+                    {
+                        Id = this.labelIdOnDb,
+                        Name = "Brute Force"
+                    }
+                }
+            };
+
+            var myNewLabels = new List<string> { "New Label" };
+            var labelService = new LabelService(this.unitOfWork);
+
+            // Act
+            var labels = labelService.GetLabelsAsync(myLabelsOnDb, myNewLabels).Result;
+
+            // Assert
+            Assert.IsTrue(labels.Count == 1);
+            Assert.IsTrue(this.addWasCalled == true);
+        }
+
+        [TestMethod]
+        public void GetLabelsAsyncAdd_AllNewDuplicateLabel()
+        {
+            // Arrange
+            var myLabelsOnDb = new List<SubdomainLabel>
+            {
+                new SubdomainLabel
+                {
+                    Label = new Label
+                    {
+                        Id = this.labelIdOnDb,
+                        Name = "Brute Force"
+                    }
+                }
+            };
+
+            var myNewLabels = new List<string> { "New Label", "New Label" };
+            var labelService = new LabelService(this.unitOfWork);
+
+            // Act
+            var labels = labelService.GetLabelsAsync(myLabelsOnDb, myNewLabels).Result;
+
+            // Assert
+            Assert.IsTrue(labels.Count == 1);
+            Assert.IsTrue(this.addWasCalled == true);
         }
     }
 }
