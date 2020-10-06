31cfc5de6c378b8cdb2496dbabffc70e2f8a36a5
Alejandro <acksecurity@hotmail.com>
2020-10-05 23:33:28 -0400

    Refactor

--- src/ReconNess.Core/Providers/IVersionProvider.cs
+++ src/ReconNess.Core/Providers/IVersionProvider.cs
@@ -4,14 +4,14 @@ using System.Threading.Tasks;
 namespace ReconNess.Core.Providers
 {
     /// <summary>
-    /// 
+    /// The interface IVersionProvider
     /// </summary>
     public interface IVersionProvider
     {
         /// <summary>
-        /// 
+        /// Obtain the latest ReconNess version released
         /// </summary>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken">otification that operations should be canceled</param>
         /// <returns></returns>
         Task<string> GetLatestVersionAsync(CancellationToken cancellationToken);
     }
--- src/ReconNess/Providers/VersionProvider.cs
+++ src/ReconNess/Providers/VersionProvider.cs
@@ -2,61 +2,58 @@
 using ReconNess.Core.Providers;
 using RestSharp;
 using System;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ReconNess.Providers
 {
     /// <summary>
-    /// 
+    /// This class implement <see cref="IVersionProvider"/>
     /// </summary>
     public class VersionProvider : IVersionProvider
     {
         private readonly IConfiguration configuration;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="VersionProvider" /> class
         /// </summary>
-        /// <param name="configuration"></param>
+        /// <param name="configuration"><see cref="IConfiguration"/></param>
         public VersionProvider(IConfiguration configuration)
         {
             this.configuration = configuration;
         }
 
         /// <summary>
-        /// 
+        /// <see cref="IVersionProvider.GetLatestVersionAsync(CancellationToken)"/>
         /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
         public async Task<string> GetLatestVersionAsync(CancellationToken cancellationToken)
         {
             var currentVersion = this.configuration["ReconNess:Version"];
-            if (string.IsNullOrEmpty(currentVersion))
+            if (!string.IsNullOrEmpty(currentVersion))
             {
-                return string.Empty;
-            }
-
-            try
-            {
-                var client = new RestClient("https://version.reconness.com/");
-                var request = new RestRequest();
-
-                var response = await client.ExecuteGetAsync(request, cancellationToken);
-
-                var match = System.Text.RegularExpressions.Regex.Match(response.Content, @"<body>\n(.*)\n</body>");
-                if (match.Success && match.Groups.Count == 2)
+                try
                 {
-                    var latestVersion = match.Groups[1].ToString();
-                    int comparison = string.Compare(currentVersion, latestVersion, comparisonType: StringComparison.OrdinalIgnoreCase);
-                    if (comparison < 0)
+                    var client = new RestClient("https://version.reconness.com/");
+                    var request = new RestRequest();
+
+                    var response = await client.ExecuteGetAsync(request, cancellationToken);
+
+                    var match = Regex.Match(response.Content, @"<body>\n(.*)\n</body>");
+                    if (match.Success && match.Groups.Count == 2)
                     {
-                        return $"[Latest v{latestVersion}]";
+                        var latestVersion = match.Groups[1].ToString();
+                        int comparison = string.Compare(currentVersion, latestVersion, comparisonType: StringComparison.OrdinalIgnoreCase);
+                        if (comparison < 0)
+                        {
+                            return $"[Latest v{latestVersion}]";
+                        }
                     }
                 }
-            }
-            catch
-            {
+                catch
+                {
 
+                }
             }
 
             return string.Empty;
