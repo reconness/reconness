339109891db8392a70bfce3be5379813e5631fb6
Alejandro <acksecurity@hotmail.com>
2020-11-10 14:45:10 -0500

    Implement agent run service

--- src/ReconNess/Services/AgentRunService.cs
+++ src/ReconNess/Services/AgentRunService.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using NLog;
 using ReconNess.Core;
@@ -34,6 +35,22 @@ namespace ReconNess.Services
         }
 
         /// <summary>
+        /// <see cref="IAgentRunService.StartOnScopeAsync(AgentRunner, string, CancellationToken)"/>
+        /// </summary>
+        public async Task StartOnScopeAsync(AgentRunner agentRunner, string channel, CancellationToken cancellationToken = default)
+        {
+            var agentRun = new AgentRun
+            {
+                Agent = agentRunner.Agent,
+                Channel = channel,
+                Description = $"Start running the agent {agentRunner.Agent.Name}",
+                Stage = Entities.Enum.AgentRunStage.RUNNING
+            };
+
+            await this.UpdateAsync(agentRun);
+        }
+
+        /// <summary>
         /// <see cref="IAgentRunService.UpdateAgentRunDoneOnScopeAsync(Agent, string, bool, bool, CancellationToken)"/>
         /// </summary>
         public async Task DoneOnScopeAsync(AgentRunner agentRunner, string channel, bool stoppedManually, bool fromException, CancellationToken cancellationToken)
@@ -43,29 +60,25 @@ namespace ReconNess.Services
                 await this.SendNotificationOnScopeAsync($"Agent {agentRunner.Agent.Name} is done!", cancellationToken);
             }
 
-            var agent = agentRunner.Agent;
-            await this.UpdateLastRunAgentOnScopeAsync(agent, cancellationToken);
+            await this.UpdateLastRunAgentOnScopeAsync(agentRunner.Agent, cancellationToken);
 
-            var agentRun = agent.AgentRuns.Where(r => r.Channel == channel)
-                .OrderBy(o => o.CreatedAt)
-                .FirstOrDefault();
-
+            var agentRun = await this.GetLastAgentRunAsync(agentRunner, channel, cancellationToken);
             if (agentRun != null)
             {
                 if (fromException)
                 {
                     agentRun.Stage = Entities.Enum.AgentRunStage.FAILED;
-                    agentRun.Description = $"The agent {agent.Name} failed";
+                    agentRun.Description = $"The agent {agentRunner.Agent.Name} failed";
                 }
                 else if (stoppedManually)
                 {
                     agentRun.Stage = Entities.Enum.AgentRunStage.STOPPED;
-                    agentRun.Description = $"The agent {agent.Name} was stopped manually";
+                    agentRun.Description = $"The agent {agentRunner.Agent.Name} was stopped manually";
                 }
                 else
                 {
                     agentRun.Stage = Entities.Enum.AgentRunStage.SUCCESS;
-                    agentRun.Description = $"The agent {agent.Name} ran successfully";
+                    agentRun.Description = $"The agent {agentRunner.Agent.Name} ran successfully";
                 }
 
                 agentRun.Logs += "\nAgent Done";
@@ -84,14 +97,30 @@ namespace ReconNess.Services
                 await this.connectorService.SendAsync(channel, "\nAgent Done", false, cancellationToken);
                 await this.connectorService.SendLogsAsync(channel, "\nAgent Done", cancellationToken);
             }
-        }
+        }        
 
         /// <summary>
         /// <see cref="IAgentRunService.InsertTerminalScopeAsync(AgentRunner, string, string, bool, CancellationToken)"/>
         /// </summary>
         public async Task InsertTerminalScopeAsync(AgentRunner agentRunner, string channel, string terminalOutput, bool includeTime = true, CancellationToken cancellationToken = default)
         {
-            await this.connectorService.SendAsync(channel, terminalOutput, includeTime, cancellationToken);
+            var agentRun = await this.GetLastAgentRunAsync(agentRunner, channel, cancellationToken);
+            if (agentRun != null)
+            {
+                using (var scope = this.serviceProvider.CreateScope())
+                {
+                    var unitOfWork =
+                        scope.ServiceProvider
+                            .GetRequiredService<IUnitOfWork>();
+
+                    agentRun.TerminalOutput += $"\n{terminalOutput}";
+
+                    unitOfWork.Repository<AgentRun>().Update(agentRun);
+                    await unitOfWork.CommitAsync();
+                }
+
+                await this.connectorService.SendAsync(channel, terminalOutput, includeTime, cancellationToken);
+            }
         }
 
         /// <summary>
@@ -99,15 +128,53 @@ namespace ReconNess.Services
         /// </summary>
         public async Task InsertLogsScopeAsync(AgentRunner agentRunner, string channel, string logs, CancellationToken cancellationToken)
         {
-            await this.connectorService.SendLogsAsync(channel, logs, cancellationToken);
+            var agentRun = await this.GetLastAgentRunAsync(agentRunner, channel, cancellationToken);
+            if (agentRun != null)
+            {
+                using (var scope = this.serviceProvider.CreateScope())
+                {
+                    var unitOfWork =
+                        scope.ServiceProvider
+                            .GetRequiredService<IUnitOfWork>();
+
+                    agentRun.Logs += $"\n{logs}";
+
+                    unitOfWork.Repository<AgentRun>().Update(agentRun);
+                    await unitOfWork.CommitAsync();
+                }
+
+                await this.connectorService.SendLogsAsync(channel, logs, cancellationToken);
+            }
         }
 
         /// <summary>
-        /// 
+        /// Obtain the last agent running base on the channel
         /// </summary>
-        /// <param name="agent"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <param name="agentRunner">The agent running</param>
+        /// <param name="channel">The channel</param>
+        /// <param name="cancellationToken">Notification that operations should be canceled</param>
+        /// <returns>The last agent running base on the channel</returns>
+        private async Task<AgentRun> GetLastAgentRunAsync(AgentRunner agentRunner, string channel, CancellationToken cancellationToken)
+        {
+            using (var scope = this.serviceProvider.CreateScope())
+            {
+                var unitOfWork =
+                    scope.ServiceProvider
+                        .GetRequiredService<IUnitOfWork>();
+
+                return await unitOfWork.Repository<AgentRun>()
+                    .GetAllQueryableByCriteria(ar => ar.Agent == agentRunner.Agent && ar.Channel == channel)
+                    .OrderBy(o => o.CreatedAt)
+                    .FirstOrDefaultAsync(cancellationToken);
+            }                            
+        }
+
+        /// <summary>
+        /// Update the latest agent run date
+        /// </summary>
+        /// <param name="agent">The agent</param>
+        /// <param name="cancellationToken">Notification that operations should be canceled</param>
+        /// <returns>A task</returns>
         private async Task UpdateLastRunAgentOnScopeAsync(Agent agent, CancellationToken cancellationToken = default)
         {
             using (var scope = this.serviceProvider.CreateScope())
@@ -137,6 +204,6 @@ namespace ReconNess.Services
 
                 await notificationService.SendAsync(payload, cancellationToken);
             }
-        }
+        }       
     }
 }
