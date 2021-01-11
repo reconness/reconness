04f902287f466b16801359c1923041f43e796a82
Alejandro <acksecurity@hotmail.com>
2021-01-11 09:36:13 -0500

    Work on rabbitmq

---
+++ src/ReconNess.PubSub/QueueAgentRunnerProvider.cs
@@ -0,0 +1,35 @@
+﻿using ReconNess.Core.Models;
+using ReconNess.Core.Providers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReconNess.PubSub
+{
+    public class QueueAgentRunnerProvider : IAgentRunnerProvider
+    {
+        public Task<int> RunningCountAsync => throw new NotImplementedException();
+
+        public Task<IList<string>> RunningChannelsAsync => throw new NotImplementedException();
+
+        public Task InitializesAsync(string channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsStoppedAsync(string channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RunAsync(AgentRunnerProviderArgs providerArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(string channel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
