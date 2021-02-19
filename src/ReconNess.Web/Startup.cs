aea99a55eeb15e9c1a48c876e921402c53a62378
Alejandro <acksecurity@hotmail.com>
2021-02-19 11:28:08 -0500

    Activate swagger for all the env

--- src/ReconNess.Web/Startup.cs
+++ src/ReconNess.Web/Startup.cs
@@ -112,16 +112,16 @@ namespace ReconNess.Web
         {
             if ("Development".Equals(Env.EnvironmentName))
             {
-                app.UseDeveloperExceptionPage();
-
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Reconness v1"));
+                app.UseDeveloperExceptionPage();                
             }
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Reconness v1"));
 
             app.UseRouting();
 
