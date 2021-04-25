c085344ff35ffe7718e419810abd0387521f429b
Alejandro <acksecurity@hotmail.com>
2021-04-25 17:25:07 -0400

    Add agent Configuration

--- src/ReconNess.Web/ClientApp/src/components/agent/AgentForm.vue
+++ src/ReconNess.Web/ClientApp/src/components/agent/AgentForm.vue
@@ -17,16 +17,16 @@
             <input name="command" formControlName="command" class="form-control" id="command" v-model="agent.command">
         </div>
         <hr />
-        <h4>Agent Type <span class="text-danger" title="What kind of Agent is this? Run in a Target, RootDomain or Subdomain?">*</span></h4>        
+        <h4>Agent Type <span class="text-danger" title="What kind of Agent is this? Run in a Target, RootDomain or Subdomain?">*</span></h4>
         <div class="form-group form-check">
             <input type="radio" id="isByRootDomain" value="RootDomain" v-model="agent.agentType">
-            <label class="form-check-label" for="isByRootDomain">
+            <label class="form-check-label ml-2" for="isByRootDomain">
                 RootDomain
             </label>
         </div>
         <div class="form-group form-check">
             <input type="radio" id="isBySubdomain" value="Subdomain" v-model="agent.agentType">
-            <label class="form-check-label" for="isBySubdomain">
+            <label class="form-check-label ml-2" for="isBySubdomain">
                 Subdomain
             </label>
         </div>
@@ -46,9 +46,9 @@
                     <label class="form-check-label" for="triggerSkipIfRunBefore">
                         Skip if Run Before
                     </label>
-                </div>                
-            </div>            
-            <div class="tab-pane fade" id="nav-rootdomain" role="tabpanel" aria-labelledby="nav-rootdomain-tab">               
+                </div>
+            </div>
+            <div class="tab-pane fade" id="nav-rootdomain" role="tabpanel" aria-labelledby="nav-rootdomain-tab">
                 <div class="row mt-4">
                     <div class="form-group col-4">
                         <label for="triggerRootdomainIncExcName">Include/Exclude RegExp RootDomain</label>
@@ -63,7 +63,7 @@
                     </div>
                 </div>
             </div>
-            <div class="tab-pane fade" id="nav-subdomain" role="tabpanel" aria-labelledby="nav-subdomain-tab">                
+            <div class="tab-pane fade" id="nav-subdomain" role="tabpanel" aria-labelledby="nav-subdomain-tab">
                 <div class="form-group form-check mt-4">
                     <input class="form-check-input" type="checkbox" id="triggerSubdomainIsAlive" v-model="agent.triggerSubdomainIsAlive">
                     <label class="form-check-label" for="triggerSubdomainIsAlive">
@@ -148,13 +148,29 @@
                     </div>
                 </div>
             </div>
-        </div>   
+        </div>
         <hr />
         <h4>Script</h4>
         <div class="form-group">
             <label for="inputArguments"><a href="https://docs.reconness.com/agents/script-agent">Learn more</a></label>
             <editor v-model="agent.script" @init="editorInit" lang="csharp" theme="dracula" width="800" height="600"></editor>
         </div>
+
+        <h4>Configuration</h4>
+        <label for="inputArguments"><a href="https://docs.reconness.com/agents/script-agent">Learn more</a></label>
+        <div class="row">
+            <div class="form-group col-4 ml-3">
+                <input type="file" id="file" ref="file" v-on:change="handleFileUpload()" />
+                <label class="custom-file-label" for="file">Import Configuration File</label>
+            </div>
+            <div class="form-group col-4">
+                <input class="form-control" id="triggerSubdomainServicePort" placeholder="File Name" />
+            </div>
+            <div class="form-group col-10">
+                <editor @init="editorConfigInit" lang="yaml" theme="dracula" width="800" height="600"></editor>
+            </div>
+        </div>
+
         <div class="form-group">
             <button class="btn btn-primary" v-if="isNew" v-on:click="onSave" :disabled='!isValid()'>Add</button>
             <button class="mt-2 btn btn-primary" v-if="!isNew" v-on:click="onUpdate()" :disabled='!isValid()'>Update</button>
@@ -244,6 +260,12 @@
                 require('brace/mode/csharp')
                 require('brace/theme/dracula')
                 require('brace/snippets/csharp')
+            },
+            editorConfigInit: function () {
+                require('brace/ext/language_tools')
+                require('brace/mode/yaml')
+                require('brace/theme/dracula')
+                require('brace/snippets/yaml')
             },
             isValid() {
                 return this.agent.name && this.agent.command && this.agent.agentType
