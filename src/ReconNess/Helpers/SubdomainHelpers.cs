416383cb818ec1164a2379b3b4689be5b7a60a22
Alejandro <acksecurity@hotmail.com>
2021-06-27 18:05:41 -0400

    Add path tranversal validation

--- src/ReconNess/Helpers/SubdomainHelpers.cs
+++ src/ReconNess/Helpers/SubdomainHelpers.cs
@@ -4,18 +4,61 @@ using System.Reflection;
 
 namespace ReconNess.Helpers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class SubdomainHelpers
     {
+        /// <summary>
+        /// Obtain the base64 screenshot if exist
+        /// </summary>
+        /// <param name="target">The target</param>
+        /// <param name="rootdomain">The rootdomain</param>
+        /// <param name="subdomain">The subdomain</param>
+        /// <returns>The base64 screenshot if exist</returns>
         public static string GetBase64Image(string target, string rootdomain, string subdomain)
         {
-            var subdomainScreenshot = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Content", "screenshots", target, rootdomain, $"{subdomain}.png");
-            if (!File.Exists(subdomainScreenshot))
+            target = RemoveInvalidFileNameChars(target);
+            rootdomain = RemoveInvalidFileNameChars(rootdomain);
+            subdomain = RemoveInvalidFileNameChars(subdomain);
+
+            var screenshotPath = GetScreenshotPath();
+            var path = Path.Combine(screenshotPath, target, rootdomain, $"{subdomain}.png");
+            if (!path.StartsWith(screenshotPath) || !File.Exists(path))
             {
                 return null;
             }
 
-            byte[] imageArray = File.ReadAllBytes(subdomainScreenshot);
+            byte[] imageArray = File.ReadAllBytes(path);
             return Convert.ToBase64String(imageArray);
+        }
+
+        /// <summary>
+        /// Obtain the screenshot folder path
+        /// </summary>
+        /// <returns>The screenshot folder path</returns>
+        private static string GetScreenshotPath()
+        {
+            var bin = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var path = Path.Combine(bin, "Content", "screenshots");
+
+            return path;
+        }
+
+        /// <summary>
+        /// Obtain the string with removed invalid file name chars
+        /// </summary>
+        /// <param name="input">The string to remove invalid file name chars</param>
+        /// <returns>The string with removed invalid file name chars</returns>
+        private static string RemoveInvalidFileNameChars(string input)
+        {
+            string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            foreach (char c in invalid)
+            {
+                input = input.Replace(c.ToString(), "");
+            }
+
+            return input;
         }
     }
 }
