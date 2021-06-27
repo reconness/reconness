fa2e36b008eec9520e92ee5c7b04f9ee525ff001
Alejandro <acksecurity@hotmail.com>
2021-06-27 17:34:34 -0400

    Work on screenshot feature

---
+++ src/ReconNess/Helpers/SubdomainHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace ReconNess.Helpers
+{
+    public static class SubdomainHelpers
+    {
+        public static string GetBase64Image(string target, string rootdomain, string subdomain)
+        {
+            var subdomainScreenshot = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Content", "screenshots", target, rootdomain, $"{subdomain}.png");
+            if (!File.Exists(subdomainScreenshot))
+            {
+                return null;
+            }
+
+            byte[] imageArray = File.ReadAllBytes(subdomainScreenshot);
+            return Convert.ToBase64String(imageArray);
+        }
+    }
+}
