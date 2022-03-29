0782b79a56883f04636ec31947c057816a54024d
Alejandro <acksecurity@hotmail.com>
2022-03-29 16:22:31 -0400

    Update packages

--- src/ReconNess/Providers/AgentRunnerQueueProvider.cs
+++ src/ReconNess/Providers/AgentRunnerQueueProvider.cs
@@ -32,8 +32,8 @@ namespace ReconNess.Providers
 
             this.channel = conn.CreateModel();
 
-            this.channel.ExchangeDeclare("reconness", ExchangeType.Direct);
-            this.channel.QueueDeclare("reconness-queue");
+            this.channel.ExchangeDeclare("reconness", ExchangeType.Direct, true);
+            this.channel.QueueDeclare("reconness-queue", true);
 
             this.channel.QueueBind("reconness-queue", "reconness", "reconness");
         }
