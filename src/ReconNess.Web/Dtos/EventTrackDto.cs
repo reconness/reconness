bb4abdc4706060705c5c84a1490e22fcd18c9dfc
Lester <lester.castilloc89@gmail.com>
2022-11-12 18:36:25 +0000

    Add the Id attribute to the EventTrackDto object

--- src/ReconNess.Web/Dtos/EventTrackDto.cs
+++ src/ReconNess.Web/Dtos/EventTrackDto.cs
@@ -5,6 +5,8 @@ namespace ReconNess.Web.Dtos
 {
     public class EventTrackDto
     {
+        public Guid Id { get; set; }
+        
         public string Description { get; set; }
 
         public string Username { get; set; }
